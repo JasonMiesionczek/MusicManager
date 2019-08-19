02c4830198406bff23ff0521842f2dae7f406de7
Jason Miesionczek <atmospherian@gmail.com>
2019-08-19 08:55:59 -0400

    docker stuff

--- server/startup.sh
+++ server/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo Xvfb :99 -screen 0 1024x768x16 &
-cargo run -p cli -- db migrate && cargo watch -x 'run --bin task-runner'
+
+Xvfb :99 -screen 0 1024x768x16 &
+/app/cli db migrate
+/app/task-runner
